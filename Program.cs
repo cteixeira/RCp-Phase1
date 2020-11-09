975cf35809db9df192befe48513b2c19b73788a1
Carlos Teixeira <cateixe@microsoft.com>
2020-11-09 23:41:34 +0000

    Add support to a new request after http 302. Start receiving URI in application args.

--- Program.cs
+++ Program.cs
@@ -2,51 +2,78 @@
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using System.Linq;
 
 namespace RCp_Phase1 {
     static class Program {
 
+        private static string HTTP_CODE_REDIRECT = "302";
+
         public static void Main(string[] args)
         {
-            string host = "127.0.0.1";
-            int port = 80;
-            string result = SocketSendReceive(host, port);
-            Console.WriteLine(result);
+            if(args == null || args.Length != 1 )
+                Console.WriteLine("An url and port must be defined in order to complete the execution.");
+
+            try
+            {
+                var uri = new Uri(args[0]);
+                string result = ManageRequest(uri, uri.Port);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"As error has ocurred: {ex}");
+            }
+
+            Console.Read();
         }
 
-        // This method requests the home page content for the specified server.
-        private static string SocketSendReceive(string server, int port)
+        private static string ManageRequest(Uri uri, int port)
         {
-            string request = "GET / HTTP/1.1\r\nHost: " + server + "\r\nConnection: Close\r\n\r\n";
-
-            Byte[] bytesSent = Encoding.ASCII.GetBytes(request);
-            Byte[] bytesReceived = new Byte[256];
-            string page = "";
-
             // Create a socket connection with the specified server and port.
-            using (Socket socket = ConnectSocket(server, port))
+            using (Socket socket = ConnectSocket(uri.Host, port))
             {
                 if (socket == null || !socket.Connected)
                     return "Connection failed";
 
-                // Send request to the server.
-                socket.Send(bytesSent, bytesSent.Length, 0);
+                string url = uri.AbsoluteUri;
+                string httpCode, response;
 
-                // Receive the server home page content.
-                int bytes = 0;
-                page = "Default HTML page on " + server + ":\r\n";
-
-                // The following will block until the page is transmitted.
                 do
                 {
-                    //TODO: RESPONSE RETURNS 302, NEED TO MAKE A NEW REQUEST
-                    bytes = socket.Receive(bytesReceived, bytesReceived.Length, 0);
-                    page += Encoding.ASCII.GetString(bytesReceived, 0, bytes);
-                }
-                while (bytes > 0);
-            }
+                    response = SendRequest(socket, uri.Host, url);
 
-            return page;
+                    var lines = response.Split("\r\n");
+                    httpCode = lines[0].Split(" ")[1];
+
+                    if (httpCode == HTTP_CODE_REDIRECT)  //make request to the new location
+                        url = lines.First(s => s.StartsWith("location", StringComparison.OrdinalIgnoreCase)).Split(" ")[1];
+
+                } while (httpCode == HTTP_CODE_REDIRECT);
+
+                return response;
+            }
+        }
+
+        private static string SendRequest(Socket socket, string host, string url) {
+
+            string request = $"GET {url} HTTP/1.1\r\nHost: {host}\r\n\r\n"; //"\r\nConnection: Close\r\n\r\n";
+
+            string response = string.Empty;
+            var bytesSent = Encoding.ASCII.GetBytes(request);
+            var bytesReceived = new byte[256];
+            int bytes;
+
+            // Send request to the server.
+            socket.Send(bytesSent, bytesSent.Length, 0);
+
+            // The following will block until the page is received.
+            do { 
+                bytes = socket.Receive(bytesReceived, bytesReceived.Length, 0);
+                response += Encoding.ASCII.GetString(bytesReceived, 0, bytes);
+            }
+            while (bytes > 0);
+
+            return response;
         }
 
         private static Socket ConnectSocket(string server, int port)
