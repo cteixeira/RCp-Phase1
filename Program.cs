ea0e743f343747b95daf42f479eaa2e95f56485c
Carlos Teixeira <cateixe@microsoft.com>
2020-11-11 00:29:17 +0000

    Added support to multiple connections in case of redirect. It now allows requests by name and IP, ex: http://www.isel.pt

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ using System.Linq;
 namespace RCp_Phase1 {
     static class Program {
 
-        private static string HTTP_CODE_REDIRECT = "302";
+        private static string HTTP_CODE_PERM_REDIRECT = "301";
+        private static string HTTP_CODE_TEMP_REDIRECT = "302";
 
         public static void Main(string[] args)
         {
@@ -17,7 +18,7 @@ namespace RCp_Phase1 {
             try
             {
                 var uri = new Uri(args[0]);
-                string result = ManageRequest(uri, uri.Port);
+                string result = ManageRequest(uri);
                 Console.WriteLine(result);
             }
             catch (Exception ex) {
@@ -27,53 +28,63 @@ namespace RCp_Phase1 {
             Console.Read();
         }
 
-        private static string ManageRequest(Uri uri, int port)
+        private static string ManageRequest(Uri uri)
         {
+            string host = resolveName(uri.Host);
+            string path = uri.AbsolutePath;
+            int port = uri.Port;
+            string httpCode, response;
+
+            do {
+                response = SendRequest(host, path, port);
+
+                var lines = response.Split("\r\n");
+                httpCode = lines[0].Split(" ")[1];
+
+                if (httpCode == HTTP_CODE_TEMP_REDIRECT || httpCode == HTTP_CODE_PERM_REDIRECT) {
+                    //make request to the new location
+                    var redirectUri = new Uri(lines.First(s => s.StartsWith("location", StringComparison.OrdinalIgnoreCase)).Split(" ")[1]);
+                    host = resolveName(redirectUri.Host);
+                    path = redirectUri.AbsolutePath;
+                    port = redirectUri.Port;
+                }
+
+            } while (httpCode == HTTP_CODE_TEMP_REDIRECT || httpCode == HTTP_CODE_PERM_REDIRECT);
+
+            return response;
+        }
+
+        private static string SendRequest(string host, string path, int port) {
+
             // Create a socket connection with the specified server and port.
-            using (Socket socket = ConnectSocket(uri.Host, port))
-            {
-                if (socket == null || !socket.Connected)
-                    return "Connection failed";
+            using (Socket socket = ConnectSocket(host, port)) {
 
-                string url = uri.AbsoluteUri;
-                string httpCode, response;
+                if (socket?.Connected != true)
+                    throw new Exception("Connection failed");
 
-                do
-                {
-                    response = SendRequest(socket, uri.Host, url);
+                // Send request to the server.
+                string request = $"GET {path} HTTP/1.1\r\nHost: {host}\r\n\r\n";
+                var bytesSent = Encoding.ASCII.GetBytes(request);
+                var bytesReceived = new byte[256];
+                socket.Send(bytesSent, bytesSent.Length, 0);
 
-                    var lines = response.Split("\r\n");
-                    httpCode = lines[0].Split(" ")[1];
-
-                    if (httpCode == HTTP_CODE_REDIRECT)  //make request to the new location
-                        url = lines.First(s => s.StartsWith("location", StringComparison.OrdinalIgnoreCase)).Split(" ")[1];
-
-                } while (httpCode == HTTP_CODE_REDIRECT);
+                // The following will block until the page is received.
+                string response = string.Empty; int bytes;
+                do {
+                    bytes = socket.Receive(bytesReceived, bytesReceived.Length, 0);
+                    response += Encoding.ASCII.GetString(bytesReceived, 0, bytes);
+                }
+                while (bytes == 256);
 
                 return response;
             }
         }
 
-        private static string SendRequest(Socket socket, string host, string url) {
-
-            string request = $"GET {url} HTTP/1.1\r\nHost: {host}\r\n\r\n"; //"\r\nConnection: Close\r\n\r\n";
-
-            string response = string.Empty;
-            var bytesSent = Encoding.ASCII.GetBytes(request);
-            var bytesReceived = new byte[256];
-            int bytes;
-
-            // Send request to the server.
-            socket.Send(bytesSent, bytesSent.Length, 0);
-
-            // The following will block until the page is received.
-            do { 
-                bytes = socket.Receive(bytesReceived, bytesReceived.Length, 0);
-                response += Encoding.ASCII.GetString(bytesReceived, 0, bytes);
-            }
-            while (bytes > 0);
-
-            return response;
+        private static string resolveName(string host) { 
+            return Dns.GetHostEntry(host)
+                                .AddressList
+                                .First(a => a.AddressFamily == AddressFamily.InterNetwork)
+                                .ToString();
         }
 
         private static Socket ConnectSocket(string server, int port)
